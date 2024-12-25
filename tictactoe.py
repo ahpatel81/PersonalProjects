222a8e0001d0d56e7fbecbfe64c36f707680db9e
ahpatel81 <165234415+ahpatel81@users.noreply.github.com>
2024-12-25 05:35:59 -0500

    Update tictactoe.py
    Update to tic tac toe game (step 3)

--- tictactoe.py
+++ tictactoe.py
@@ -81,7 +81,7 @@ class GameBoard(tk.Tk):
 
 
 class TicTacToeGame:
-    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+    def __init__(self, players = DEFAULT_PLAYERS, board_size = BOARD_SIZE):
         self.player = cycle(players)
         self.board_size = board_size
         self.current_player = next(self.player)
@@ -107,6 +107,44 @@ class TicTacToeGame:
         first_diagonal = [row[i] for i, row in enumerate(rows)]
         second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
         return rows + columns + [first_diagonal, second_diagonal]
+    
+    def is_valid_move(self, move):
+        """Return True if move is valid, and False otherwise."""
+        row, col = move.row, move.col
+        move_was_not_played = self.current_moves[row][col].label == ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+    
+    def process_move(self, move):
+        """Process the current move and check if it's a win."""
+        row, col = move.row, move.col
+        self.current_moves[row][col] = move
+        for combo in self.winning_combos:
+            results = set(
+                self.current_moves[n][m].label
+                for n, m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+    
+    def has_winner(self):
+        """Return True if the game has a winner, and False otherwise."""
+        return self._has_winner
+    
+    def is_tied(self):
+        """Return True if the game is tied, and False otherwise."""
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self.current_moves for move in row
+        )
+        return no_winner and all(played_moves)
+    
+    def toggle_player(self):
+        """Return a toggled player."""
+        self.current_player = next(self.players)
 
 
 def main():
